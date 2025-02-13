4afff3a7831f7127afa0fd7da23c45240cc9a337
mithridatem <mathieumithridate@adrar-formation.com>
2025-02-13 23:24:38 +0100

    feat: livre gui

---
+++ src/Gui/LivreForm.java
@@ -0,0 +1,14 @@
+package Gui;
+
+import javax.swing.*;
+
+public class LivreForm {
+    private JPanel JpLivreMain;
+    private JTextField tfTitre;
+    private JTextField tfDate;
+    private JLabel jlDate;
+    private JLabel jlTitre;
+    private JButton btCancel;
+    private JButton batAdd;
+    private JButton btGenre;
+}
